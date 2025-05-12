2ba37a00272230884a3ce201d501f0245a857f81
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-12 21:57:02 +0200

    added useEffect

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,6 +1,15 @@
+import {useEffect} from "react";
+import axios from "axios";
+import s from './CharacterPage.module.css'
 
 
 export const CharacterPage = () => {
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character') .then(res => {
+            console.log(res)
+        })
+    }, [])
+
     return (
         <div>
             <h1 className={'pageTitle'}>CharacterPage</h1>
