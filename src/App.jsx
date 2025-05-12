fab477cdfddc044e3b75350c17f639a794a6550f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-12 23:08:35 +0200

    added charactersID Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {LocationPage} from "./pages/LocationPage/LocationPage.jsx";
 import {EpisodePage} from "./pages/EpisodePage/EpisodePage.jsx";
 import {Header} from "./common/components/Header.jsx";
 import {Route, Routes} from "react-router";
+import {Character} from "./pages/CharacterPage/Character/Character.jsx";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/characters" element={<CharacterPage/>}/>
+                <Route path="/characters/:id" element={<Character />} />
                 <Route path="/locations" element={<LocationPage/>}/>
                 <Route path="/episodes" element={<EpisodePage/>}/>
             </Routes>
