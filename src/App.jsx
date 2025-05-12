c92572eb544352b4982ac85474c6d0080ad87ac1
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-12 18:19:49 +0200

    added LocationPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {HomePage} from "./pages/HomePage/HomePage.jsx";
 import {CharacterPage} from "./pages/CharacterPage/CharacterPage.jsx";
+import {LocationPage} from "./pages/LocationPage/LocationPage.jsx";
 
 function App() {
     return (
         <div>
             <HomePage />
             <CharacterPage />
+            <LocationPage />
         </div>
     )
 }
