c273db612c85ccb0af22f104c4dc7536c6f518a0
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-12 18:57:29 +0200

    added styles for HomePageTitle

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import {NavLink} from "react-router";
+import s from './HomePage.module.css'
 
 
 export const HomePage = () => {
     return (
         <div>
-            <h1>The Rick and Morty</h1>
+            <h1 className={s.title}>The Rick and Morty</h1>
             <div>
                 <NavLink to={'/characters'}>Characters</NavLink>
                 <NavLink to={'/locations'}>Locations</NavLink>
