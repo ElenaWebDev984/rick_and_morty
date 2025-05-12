34cce6256d849fa57dbf86fcc71685af3f3e714f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-12 18:51:29 +0200

    added NavLinks

--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -1,9 +1,16 @@
 import logo from '../../assets/img/logo.png'
+import {NavLink} from "react-router";
 
 export const Header = () => {
     return (
         <div>
-            <img src={logo} alt="logotype"/>
+            <NavLink to={'/'}>
+                <img src={logo} alt="logotype"/>
+            </NavLink>
+            <NavLink to={'/'}>Home</NavLink>
+            <NavLink to={'/characters'}>Characters</NavLink>
+            <NavLink to={'/locations'}>Locations</NavLink>
+            <NavLink to={'/episodes'}>Episodes</NavLink>
         </div>
     );
 };
