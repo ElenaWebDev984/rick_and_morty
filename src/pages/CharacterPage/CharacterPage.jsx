0de91503dcb958d98ce36ede01e104ad369fe249
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-12 22:11:22 +0200

    added useState

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,19 +1,20 @@
-import {useEffect} from "react";
-import axios from "axios";
-import s from './CharacterPage.module.css'
-
+import { useEffect, useState } from "react"
+import axios from "axios"
+import s from "./CharacterPage.module.css"
 
 export const CharacterPage = () => {
+    const [characters, setCharacters] = useState([])
+
     useEffect(() => {
-        axios.get('https://rickandmortyapi.com/api/character') .then(res => {
-            console.log(res)
+        axios.get("https://rickandmortyapi.com/api/character").then((res) => {
+            setCharacters(res.data.results)
         })
     }, [])
 
     return (
         <div>
-            <h1 className={'pageTitle'}>CharacterPage</h1>
+            <h1 className={"pageTitle"}>CharacterPage</h1>
+            <div>{characters[0].name}</div>
         </div>
-    );
-};
-
+    )
+}
