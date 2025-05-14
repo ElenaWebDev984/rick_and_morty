152789e66242467f05c0c127dcbd5fd60210a7b5
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-15 00:51:49 +0200

    added map for episodeArr

--- src/pages/EpisodePage/EpisodePage.jsx
+++ src/pages/EpisodePage/EpisodePage.jsx
@@ -1,9 +1,38 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 export const EpisodePage = () => {
+
+    const [episodes, setEpisodes] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/episode').then((res) => {
+            console.log(res.data);
+            setEpisodes(res.data.results);
+        })
+    })
+
     return (
-        <div>
+        <div className={'pageContainer'}>
             <h1 className={'pageTitle'}>EpisodePage</h1>
+            {episodes.length && (
+                <div>
+                    {episodes.map((episode) => {
+                        return (
+                            <div key={episode.id}>
+                                <ul>
+                                    <li>Episode name: <b>{episode.name}</b></li>
+                                    <li>Date of episode aired: <b>{episode.air_date}</b></li>
+                                    <li>Episode: <b>{episode.episode}</b></li>
+                                    <li>List of characters who were seen in the episode: <b>{episode.characters.length}</b></li>
+                                </ul>
+                                <hr/>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
         </div>
     );
 };
